ab8ea57085b0b4b09b7c04426a79b6cae5c9c55b
Emanuel Coletti <emanuelcoletti001@gmail.com>
2024-10-22 18:24:26 -0300

    comentario de lineas

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import turtle
-import time
-import random
+import turtle #Proporciona las herramientas graficas para dibujar en la pantalla
+import time #Para controlar el tiempo,por ejemplo, pausas en el juego.
+import random # Para generar numeros aleatorios,como las posiciones de la comida
 
