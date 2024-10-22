a2d2e40a5d5da30692ea738cde669f5dd64dbff1
Emanuel Coletti <emanuelcoletti001@gmail.com>
2024-10-22 19:00:16 -0300

    creando un loop para que se mantegna la ventana abierta para testear el juego

--- main.py
+++ main.py
@@ -9,3 +9,9 @@ window.title("NeoSnake2")               # Asigna el titulo de la ventana
 window.bgcolor("black")                 # Establecer el color de fondo a negro
 window.setup(width=600, height=600)     # Define el tamaño de la ventana a 600x600 pixeles
 window.tracer(0)                        # Desactiva la actualización automática de la pantalla
+
+
+
+
+# Mantener la ventana abierta
+window.mainloop()  # Este metodo mantiene la ventana abierta
