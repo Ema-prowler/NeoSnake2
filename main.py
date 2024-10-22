7dc15852e4879ce634dc460affdab52b10069ae8
Emanuel Coletti <emanuelcoletti001@gmail.com>
2024-10-22 18:28:23 -0300

    creando la ventana del juego

--- main.py
+++ main.py
@@ -2,3 +2,6 @@ import turtle #Proporciona las herramientas graficas para dibujar en la pantalla
 import time #Para controlar el tiempo,por ejemplo, pausas en el juego.
 import random # Para generar numeros aleatorios,como las posiciones de la comida
 
+#Configuracion de la ventana
+
+window = turtle.Screen() # Crea la ventana del juego
