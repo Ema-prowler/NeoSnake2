9ab478b4ec46496d4fba65f6b126e154b65e5b10
Emanuel Coletti <emanuelcoletti001@gmail.com>
2024-10-22 19:55:53 -0300

    Creando el ciclo principal, actualizando la pantalla en cada iteracion, comentando la linea del mainloop ya no necesaria

--- main.py
+++ main.py
@@ -22,5 +22,10 @@ cabeza.penup()                          # Evita que la cabeza dibuje lineas mien
 cabeza.goto(0,0)                  # Posiciona la cabeza en el centro de la pantalla.
 cabeza.direction = "stop"               # La cabeza comienza detenida (Sin direccion).
 
+while True:
+    window.update()                     # Actualiza la pantalla en cada iteracion.
+
+
+
 # Mantener la ventana abierta
-window.mainloop()  # Este metodo mantiene la ventana abierta
+#window.mainloop()  # Este metodo mantiene la ventana abierta
